372a12d87c83eea05034290c3cb916bd08e9fd1b
Robert Podrebarac <78316350+rpodrebarac@users.noreply.github.com>
2021-10-10 14:55:46 -0400

    Preliminary Navigation Bar

---
+++ src/components/view-home/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div id="home-grid">
+            <h1 id="home-grid__title">Explore Blogs</h1>
+            <div>
+                This is where the Hello, World blog will be located.
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/view-navigation-bar/navigation-bar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavigationBar() {
+    return (
+        <nav>
+            <a href="/" id="navigation-bar__header">Neutron Blog</a>
+            <span id="navigation-bar__button">&#8801;</span>
+        </nav>
+    )
+}
+
+export default NavigationBar;
