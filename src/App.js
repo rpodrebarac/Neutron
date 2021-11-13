40e953e20271678a2735719412ffb117d5ead184
Robert Podrebarac <78316350+rpodrebarac@users.noreply.github.com>
2021-11-13 18:42:10 -0500

    Installed React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/home";
 import NavigationBar from "./components/navigation-bar/navigation-bar";
 
 function App() {
     return (
-        <div>
-            <NavigationBar />
-            <Home />
-        </div>
+        <BrowserRouter>
+            <div>
+                <NavigationBar />
+
+                {/* Only one route shows at a time. */}
+                <Routes>
+                    {/* Each individual route or path. */}
+                    <Route path="/" element={ <Home /> }></Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
