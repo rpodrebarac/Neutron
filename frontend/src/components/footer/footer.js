092707e614b20985fa007bac1464bc37432ff339
Robert Podrebarac <78316350+rpodrebarac@users.noreply.github.com>
2021-12-29 16:04:21 -0500

    Styling for the Contributors Link on the Blog's Footer

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -8,9 +8,13 @@ function Footer() {
             <hr id="footer-line" />
             <div id="footer-grid__container">
                 <Link to="/" id="footer-header" className="footer-grid__item"><span style={{color: "#667ba6"}}>Neutron</span> Blog</Link>
-                <span id="footer-links">
+                <span class="footer-links">
                     <Link to="/mathematics" className="links">Mathematics</Link> | <Link to="/physics" className="links">Physics</Link> | <Link to="/chemistry" className="links">Chemistry</Link> | <Link to="/web-development" className="links">Web Development</Link> | <Link to="/other" className="links">Other</Link>
                 </span>
+    
+                <div class="footer-links extra-top-padding">
+                    <Link to="/contribute" className="links">Contributors</Link>
+                </div>
             </div>
         </div>
     )
